a02445454a6d1c2738185d93c4c92f61f189e90a
Carlos Teller <carlos.z.teller@gmail.com>
2020-05-11 18:03:07 -0500

    Added jQuery and Bootstrap; added head, navbar and footer templates; completed the Roll Dice and Posts exercises

--- src/main/java/com/codeup/springblogapp/HomeController.java
+++ src/main/java/com/codeup/springblogapp/HomeController.java
@@ -1,14 +1,47 @@
 package com.codeup.springblogapp;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    @ResponseBody
-    public String landingPage() {
-        return "This is the landing page!";
+    public String home(Model model) {
+        model.addAttribute("title", "This is my homepage!");
+        return "home";
+    }
+
+    @GetMapping("/{name}")
+    public String sayHello(@PathVariable String name, Model model) {
+        model.addAttribute("name", name);
+        return "home";
+    }
+
+    @PostMapping("/")
+    public String postSayHello(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name", "Hello, " + name + "!");
+        return "home";
+    }
+
+    @GetMapping("/roll-dice")
+    public String rollDice(Model model) {
+        model.addAttribute("title", "Roll Dice");
+        return "roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{dice}")
+    public String getDiceRoll(@PathVariable int dice, Model model) {
+        model.addAttribute("title", "Dice Roll Results");
+
+        int random = (int) Math.floor((Math.random() * 6) + 1);
+
+        if(dice == random) {
+            model.addAttribute("result", "You chose: " + dice + ". The dice landed on: " + random + ". You got it right!");
+        } else {
+            model.addAttribute("result", "You chose: " + dice + ". The dice landed on: " + random + ". Sorry! Wrong guess...");
+        }
+
+        return "roll-dice";
     }
 }
--- src/main/java/com/codeup/springblogapp/PostController.java
+++ src/main/java/com/codeup/springblogapp/PostController.java
@@ -1,6 +1,7 @@
 package com.codeup.springblogapp;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,15 +10,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class PostController {
     @GetMapping("/posts")
-    @ResponseBody
-    public String posts() {
-        return "posts index page";
+    public String posts(Model model) {
+        model.addAttribute("title", "View All Posts");
+        return "/posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String postById(@PathVariable long id) {
-        return "view an individual post: id: " + id;
+    public String postById(@PathVariable long id, Model model) {
+        Post post = new Post(id, "My Post Title", "Hello! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.");
+
+        model.addAttribute("title", "View Single Post");
+        model.addAttribute("post", post);
+
+        return "/posts/show";
     }
 
     @GetMapping("/posts/create")

65ecc38e3d457531bf8411bd2cf9cb0e487c10e4
Carlos Teller <carlos.z.teller@gmail.com>
2020-05-11 12:24:35 -0500

    Cleaned up the math controller to utilize one method instead of multiple methods

--- src/main/java/com/codeup/springblogapp/MathController.java
+++ src/main/java/com/codeup/springblogapp/MathController.java
@@ -7,27 +7,44 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MathController {
-    @GetMapping("/add/{num1}/and/{num2}")
-    @ResponseBody
-    public String add(@PathVariable int num1, @PathVariable int num2) {
-        return num1 + " + " + num2 + " = " + (num1 + num2);
-    }
+//    @GetMapping("/add/{num1}/and/{num2}")
+//    @ResponseBody
+//    public String add(@PathVariable int num1, @PathVariable int num2) {
+//        return num1 + " + " + num2 + " = " + (num1 + num2);
+//    }
+//
+//    @GetMapping("/subtract/{num1}/from/{num2}")
+//    @ResponseBody
+//    public String subtract(@PathVariable int num1, @PathVariable int num2) {
+//        return num2 + " - " + num1 + " = " + (num2 - num1);
+//    }
+//
+//    @GetMapping("/multiply/{num1}/and/{num2}")
+//    @ResponseBody
+//    public String multiply(@PathVariable int num1, @PathVariable int num2) {
+//        return num1 + " * " + num2 + " = " + (num1 * num2);
+//    }
+//
+//    @GetMapping("/divide/{num1}/by/{num2}")
+//    @ResponseBody
+//    public String divide(@PathVariable int num1, @PathVariable int num2) {
+//        return num1 + " / " + num2 + " = " + (num1 / num2);
+//    }
 
-    @GetMapping("/subtract/{num1}/from/{num2}")
+    @GetMapping("/{operation}/{num1}/{keyword}/{num2}")
     @ResponseBody
-    public String subtract(@PathVariable int num1, @PathVariable int num2) {
-        return num2 + " - " + num1 + " = " + (num2 - num1);
-    }
-
-    @GetMapping("/multiply/{num1}/and/{num2}")
-    @ResponseBody
-    public String multiply(@PathVariable int num1, @PathVariable int num2) {
-        return num1 + " * " + num2 + " = " + (num1 * num2);
-    }
-
-    @GetMapping("/divide/{num1}/by/{num2}")
-    @ResponseBody
-    public String divide(@PathVariable int num1, @PathVariable int num2) {
-        return num1 + " / " + num2 + " = " + (num1 / num2);
+    public String math(@PathVariable String operation, @PathVariable int num1, @PathVariable String keyword, @PathVariable int num2) {
+        switch (operation) {
+            case "add":
+                return num1 + " + " + num2 + " = " + (num1 + num2);
+            case "subtract":
+                return num2 + " - " + num1 + " = " + (num2 - num1);
+            case "multiply":
+                return num1 + " * " + num2 + " = " + (num1 * num2);
+            case "divide":
+                return num1 + " / " + num2 + " = " + (num1 / num2);
+            default:
+                return "Math not good...";
+        }
     }
 }
